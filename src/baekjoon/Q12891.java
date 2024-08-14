915c50d2cdb6bc25b7e38d00d1748488e366ef90
Lee-HyunSoo <cris_lx@naver.com>
2024-08-15 01:33:59 +0900

    boj

---
+++ src/baekjoon/Q12891.java
@@ -0,0 +1,95 @@
+package baekjoon;
+
+import java.util.*;
+
+/**
+ * DNA 비밀번호
+ *
+ * 1. 입력부
+ *   1-1. 문자열의 길이 s, 부분 문자열의 길이 p, 문자열 입력
+ *   1-2. 부분 문자열에 포함되어야 할 {A, C, G, T} 의 최소 개수
+ *   1-3. 문자를 세기위한 words 배열
+ *
+ * 2. start, end 를 잡고 부분문자열을 하나씩 늘려간다.
+ *   2-1. 부분 문자열의 길이가 주어지기 때문에, end = start + p - 1
+ *   2-2. start, end 를 사용해 부분문자열을 구한다.
+ *   2-3. 부분 문자열을 통해 문자의 개수를 count 한다.
+ *   2-4. end 가 s 보다 작을 때까지 반복한다.
+ *   2-5. 매번 문자열을 자르면 시간초과가 나기 때문에, 처음에만 자르고 이후 한칸씩만 count 를 줄이고 늘리며 비교한다.
+ */
+public class Q12891 {
+
+    static int[] minCount;
+    static int[] words;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int s = scan.nextInt();
+        int p = scan.nextInt();
+        scan.nextLine();
+        String dna = scan.nextLine();
+
+        minCount = new int[4]; // A C G T
+        words = new int[4];
+        for (int idx = 0; idx < 4; idx++) {
+            minCount[idx] = scan.nextInt();
+        }
+
+        int answer = 0;
+        int start = 0;
+        // 2-1. 부분 문자열의 길이가 주어지기 때문에, end = start + p
+        int end = start + p;
+        // 2-2. start, end 를 사용해 부분문자열을 구한다.
+        String subString = dna.substring(start, end);
+        // 2-3. 부분 문자열을 통해 문자의 개수를 count 한다.s
+        for (char word : subString.toCharArray()) {
+            add(word);
+        }
+        if (isPassword()) {
+            answer++;
+        }
+
+        // 2-4. end 가 s 보다 작을 때까지 반복한다.
+        while (end < s) {
+            // 2-5. 매번 문자열을 자르면 시간초과가 나기 때문에, 처음에만 자르고 이후 한칸씩만 줄이고 늘리며 비교한다.
+            remove(dna.charAt(start++));
+            add(dna.charAt(end++));
+            if (isPassword()) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static void add(char word) {
+        if (word == 'A') {
+            words[0]++;
+        } else if (word == 'C') {
+            words[1]++;
+        } else if (word == 'G') {
+            words[2]++;
+        } else if (word == 'T') {
+            words[3]++;
+        }
+    }
+
+    private static void remove(char word) {
+        if (word == 'A') {
+            words[0]--;
+        } else if (word == 'C') {
+            words[1]--;
+        } else if (word == 'G') {
+            words[2]--;
+        } else if (word == 'T') {
+            words[3]--;
+        }
+    }
+
+    private static boolean isPassword() {
+        return words[0] >= minCount[0]
+                && words[1] >= minCount[1]
+                && words[2] >= minCount[2]
+                && words[3] >= minCount[3];
+    }
+}
---
+++ src/baekjoon/Q2961.java
@@ -0,0 +1,71 @@
+package baekjoon;
+
+import java.util.*;
+
+/**
+ * 도영이가 만든 맛있는 음식
+ *
+ * 1. 입력부
+ *  1-1. n 개의 재료, 신맛 s 와 쓴맛 b
+ *  1-2. 신맛: 사용한 재료들의 s 곱
+ *  1-3. 쓴맛: 사용한 재료들의 b 합
+ *
+ * 2. 부분집합 사용
+ *  2-1. level, idx, 신맛의 곱, 쓴맛의 합을 파라미터로 넘긴다.
+ *  2-2. 연산을 진행하고 넘어가는 재귀
+ *  2-3. 연산을 하지 않고 넘어가는 재귀
+ *
+ * 3. 종료 조건
+ *  3-1. level 이 n 이되면 종료한다.
+ *  3-2. sour, bitter 가 초기값이 아닐 때 신맛의 곱과 쓴맛의 합의 차이가 가장 적은 것으로 갱신한다.
+ */
+public class Q2961 {
+
+    static class Pair {
+        int sour, bitter;
+
+        Pair(int sour, int bitter) {
+            this.sour = sour;
+            this.bitter = bitter;
+        }
+    }
+
+    static int n;
+    static List<Pair> foods;
+    static int answer;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // 1. 입력부
+        n = scan.nextInt();
+        foods = new ArrayList<>();
+        answer = Integer.MAX_VALUE;
+        for (int idx = 0; idx < n; idx++) {
+            int sour = scan.nextInt();
+            int bitter = scan.nextInt();
+            foods.add(new Pair(sour, bitter));
+        }
+
+        // 2. 부분집합 사용
+        subset(0, 1, 0);
+        System.out.println(answer);
+    }
+
+    // 2-1. level, idx 를 파라미터로 넘긴다.
+    private static void subset(int level, int sour, int bitter) {
+        // 3-1. level 이 n 이되면 종료한다.
+        if (level == n) {
+            // 3-2. sour, bitter 가 초기값이 아닐 때 신맛의 곱과 쓴맛의 합의 차이가 가장 적은 것으로 갱신한다.
+            if (sour != 1 && bitter != 0) {
+                answer = Math.min(answer, Math.abs(sour - bitter));
+            }
+            return;
+        }
+
+        // 2-2. 연산을 진행하고 넘어가는 재귀
+        subset(level + 1, sour * foods.get(level).sour, bitter + foods.get(level).bitter);
+        // 2-3. 연산을 하지 않고 넘어가는 재귀
+        subset(level + 1, sour, bitter);
+    }
+}
