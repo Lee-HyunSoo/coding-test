81618ea93e2521fabebf1416d3003c8071dee92f
Lee-HyunSoo <cris_lx@naver.com>
2024-08-16 01:52:10 +0900

    g5

--- src/baekjoon/Q2023.java
+++ src/baekjoon/Q2023.java
@@ -5,84 +5,70 @@ import java.util.Scanner;
 /**
  * 신기한 소수
  *
- * 1. 입력부
+ * 1. 입력부, 조건
  *   1-1. 7331 -> 7, 73, 733, 7331 모두 소수여야 한다.
- *   1-2. min = 10^(n - 1), max = 10^n
  *
  * 2. 소수의 특징
- *   2-1. 한자리는 2, 3, 5, 7 중 하나가 아니면 소수가 아니다.
- *   2-2. 두자리 이상의 수의 경우 1, 3, 7, 9 로만 끝날 수 있다.
+ *   2-1. 짝수는 소수가 아니다.
+ *   2-2. 한자리는 2, 3, 5, 7 중 하나가 아니면 소수가 아니다.
+ *   2-3. 두자리 이상의 수의 경우 1, 3, 7, 9 로만 끝날 수 있다.
  *
  * 3. 판별
- *   3-1. 10 미만의 수라면 맨 앞자리 수가 2, 3, 5, 7 인지 확인한다.
- *   3-2. 10 이상의 수라면 우선 0 ~ 2 까지 문자열을 자른다.
- *   3-3. 슬라이딩 윈도우를 활용하기 위해 end 를 사용한다.
- *   3-4. 우선 자른 문자열의 맨 앞이 2, 3, 5, 7 인지 확인한다.
- *   3-5. 이후 end 가 1, 3, 7, 9 인지 확인한다.
- *   3-6. 만약 end 가 1, 3, 7, 9 라면 소수임을 판별한다.
- *     3-6-1. 넘어온 수가 짝수면 소수가 아니다.
- *     3-6-2. 홀수라면 3 ~ 루트 num 까지 나눠보며 소수임을 판별한다.
+ *   3-1. 메모리 초과를 방지하기 위해 StringBuilder 를 사용한다.
+ *   3-2. {2, 3, 5, 7} 중 하나를 맨 앞에 넣는다.
+ *   3-3. {1, 3, 7, 9} 를 통해 중복순열을 구한다.
+ *
+ * 4. 종료 조건
+ *   4-1. 부분수열 또한 소수여야 하기 때문에, 부분수열이 소수가 아니라면 return 한다.
+ *   4-2. 맨 앞의 수를 이미 정했기 때문에 n-1 크기의 중복순열을 구한다.
+ *   4-3. n-1 크기의 중복순열을 구했다면, 소수여부를 판단하고 출력 후 return 한다.
  */
 public class Q2023 {
 
+    static int n;
+    static int[] firstNum = {2, 3, 5, 7};
+    static int[] lastNum = {1, 3, 7, 9};
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
 
-        int n = scan.nextInt();
-        int min = (int) Math.pow(10, n - 1);
-        int max = (int) Math.pow(10, n);
-
-        Loop:
-        for (int num = min; num < max; num++) {
-            String parseNum = String.valueOf(num);
-
-            // 3-1. 10 미만의 수라면 맨 앞자리 수가 2, 3, 5, 7 인지 확인한다.
-            if (parseNum.length() == 1 && !first(parseNum.charAt(0))) {
-                continue;
-            }
-
-            // 3-2. 10 이상의 수라면 우선 0 ~ 1 까지 문자열을 자른다.
-            int end = 1;
-            StringBuilder subNum = new StringBuilder(parseNum.substring(0, end));
-            while (end < n) {
-                // 3-3. 슬라이딩 윈도우를 활용하기 위해 end 를 사용한다.
-                subNum.append(parseNum.charAt(end));
-
-                // 3-4. 우선 자른 문자열의 맨 앞이 2, 3, 5, 7 인지 확인한다.
-                if (!first(subNum.charAt(0))) {
-                    continue Loop;
-                }
-
-                // 3-5. 이후 end 가 1, 3, 7, 9 인지 확인한다.
-                if (!last(subNum.charAt(end))) {
-                    continue Loop;
-                }
-
-                // 3-6. 만약 end 가 1, 3, 7, 9 라면 소수임을 판별한다.
-                if (!isPrime(Integer.parseInt(subNum.toString()))) {
-                    continue Loop;
-                }
-                end++;
-            }
-            System.out.println(num);
+        for (int idx = 0; idx < 4; idx++) {
+            // 3-1. 메모리 초과를 방지하기 위해 StringBuilder 를 사용한다.
+            StringBuilder subNum = new StringBuilder();
+            // 3-2. {2, 3, 5, 7} 중 하나를 맨 앞에 넣는다.
+            subNum.append(firstNum[idx]);
+            permutation(0, subNum);
         }
     }
 
-    private static boolean first(char num) {
-        // 2-1. 한자리는 2, 3, 5, 7 중 하나가 아니면 소수가 아니다.
-        return num == '2' || num == '3' || num == '5' || num == '7';
+    private static void permutation(int level, StringBuilder subNum) {
+        // 4-1. 부분수열 또한 소수여야 하기 때문에, 부분수열이 소수가 아니라면 return 한다.
+        if (!isPrime(subNum)) {
+            return;
+        }
+
+        // 4-2. 맨 앞의 수를 이미 정했기 때문에 n-1 크기의 중복순열을 구한다.
+        // 4-3. n-1 크기의 중복순열을 구했다면, 소수여부를 판단하고 출력 후 return 한다.
+        if (level == n - 1) {
+            if (isPrime(subNum)) {
+                System.out.println(subNum);
+                return;
+            }
+        }
+
+        // 3-3. {1, 3, 7, 9} 를 통해 중복순열을 구한다.
+        for (int i = 0; i < 4; i++) {
+            subNum.append(lastNum[i]);
+            permutation(level + 1, subNum);
+            subNum.delete(subNum.length() - 1, subNum.length());
+        }
     }
 
-    private static boolean last(char num) {
-        // 2-2. 두자리 이상의 수의 경우 1, 3, 7, 9 로만 끝날 수 있다.
-        return num == '1' || num == '3' || num == '7' || num == '9';
-    }
-
-    private static boolean isPrime(int num) {
-        // 3-6-1. 넘어온 수가 짝수면 소수가 아니다.
-        // 3-6-2. 홀수라면 3 ~ 루트 num 까지 나눠보며 소수임을 판별한다.
-        for (int div = 2; div <= Math.sqrt(num); div++) {
-            if (num % div == 0) {
+    private static boolean isPrime(StringBuilder subNum) {
+        int num = Integer.parseInt(subNum.toString());
+        for (int idx = 2; idx <= Math.sqrt(num); idx++) {
+            if (num % idx == 0) {
                 return false;
             }
         }
